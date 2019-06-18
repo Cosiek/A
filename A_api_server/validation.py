dfa87746b2506e025642f253d751aedae61c479f
cosiek <fitek2@o2.pl>
2019-06-18 23:37:11 +0200

    Add timestamp validation

--- A_api_server/validation.py
+++ A_api_server/validation.py
@@ -29,14 +29,24 @@ async def validate(request):
 
     device_id = params.get("id")
     device_signature = params.get("signature")
+    device_timestamp = params.get("timestamp")
 
-    if device_id is None or device_signature is None:
+    if None in [device_id, device_signature, device_timestamp]:
+        return False, {'text': "Bad Request", 'status': 400}, None
+
+    if not (isinstance(device_timestamp, int)
+            or device_timestamp < 1560893551960):
         return False, {'text': "Bad Request", 'status': 400}, None
 
     # get device data from db
-    device = {"id": device_id, "key": "123"}  # mock - for now
+    device = {"id": device_id, "key": "123", "timestamp": 0}  # mock - for now
     if device is None:
         return False, {'text': "Not Found", 'status': 404}, None
+
+    # check if incoming timestamp is later then last recieved one
+    if device["timestamp"] >= device_timestamp:
+        return False, {'text': "Bad Request", 'status': 400}, None
+
     # generate signature based on passed data
     local_signature = get_signature(params, device["key"])
 
