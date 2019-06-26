0e10ac3b8dd39d1546c08a78ed09c9aa918e59d8
cosiek <fitek2@o2.pl>
2019-06-27 00:11:12 +0200

    Use a settings file to keep constancess across whole app

---
+++ A/Settings.js
@@ -0,0 +1,6 @@
+
+const DEBUG = permanentSettings.getIsDebug();
+
+const PROTOCOL = DEBUG ? "http" : "https";
+// TODO: what is my domain?
+const SERVER_DOMAIN = PROTOCOL + (DEBUG ? "://127.0.0.1:8080" : "TODO");
