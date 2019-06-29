e7a0e9f629425d8b154bb81ae8cff5a3b0d334da
cosiek <fitek2@o2.pl>
2019-06-29 02:02:38 +0200

    Add initial firm, vehicle and driver models

--- A_api_server/models.py
+++ A_api_server/models.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
 
 from db import Base
 
@@ -13,3 +14,28 @@ class Device(Base):
     name = Column(String(13), unique=True)
     key = Column(String(32))
     timestamp = Column(Integer)
+
+
+class Vehicle(Base):
+    __tablename__ = 'vehicles'
+
+    id = Column(Integer, primary_key=True)
+    vin = Column(String(13), unique=True)
+    name = Column(String(13), unique=True)
+
+
+class Firm(Base):
+    __tablename__ = 'firms'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(40), unique=True)
+
+
+class Driver(Base):
+    __tablename__ = 'drivers'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(40))
+
+    firm_id = Column(Integer, ForeignKey('firms.id'))
+    firm = relationship("Firm", back_populates="drivers")
