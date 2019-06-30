01df99fe5d18fb9bd05bdcd5f396e5ea39b9f076
cosiek <fitek2@o2.pl>
2019-07-01 00:57:09 +0200

    Fix models mapping relationships

--- A_api_server/models.py
+++ A_api_server/models.py
@@ -16,10 +16,10 @@ class Device(Base):
     timestamp = Column(Integer)
 
     vehicle_id = Column(Integer, ForeignKey('vehicles.id'))
-    vehicle = relationship("Vehicle", uselist=False, back_populates="vehicle")
+    vehicle = relationship("Vehicle", uselist=False, back_populates="devices")
 
     firm_id = Column(Integer, ForeignKey('firms.id'))
-    firm = relationship("Firm", back_populates="vehicles")
+    firm = relationship("Firm", back_populates="devices")
 
 
 class Vehicle(Base):
@@ -30,11 +30,18 @@ class Vehicle(Base):
     name = Column(String(13), unique=True)
 
 
+Vehicle.devices = relationship("Device", back_populates="vehicle")
+
+
 class Firm(Base):
     __tablename__ = 'firms'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(40), unique=True)
+
+
+Firm.devices = relationship("Device", back_populates="firm")
+Firm.drivers = relationship("Driver", back_populates="firm")
 
 
 class Driver(Base):
