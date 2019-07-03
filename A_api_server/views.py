60a41f71de54f2ee52ee3d73bd30facce970a685
cosiek <fitek2@o2.pl>
2019-07-03 22:18:54 +0200

    Add view to retrieve list of drivers (wip)

--- A_api_server/views.py
+++ A_api_server/views.py
@@ -16,3 +16,33 @@ async def device_register(request):
     is_valid, response_kwargs, _, _ = await validate(request, ses)
     ses.close()
     return web.Response(**response_kwargs)
+
+
+# Driver login ------------------------
+
+async def drivers(request):
+    """
+    Returns a list of drivers associated with devices firm and some data
+    needed for driver login.
+    """
+    session = Session()
+    # validate request
+    is_valid, response_kwargs, device, params = await validate(request, session)
+    if not is_valid:
+        session.close()
+        return web.Response(**response_kwargs)
+    # get list of drivers working for devices firm
+    drivers = session.query(Driver).filter_by(firm_id=device.firm_id)
+    # serialize data
+    d = {
+        "last": None,  # TODO: pass
+        "list": [],
+        "passwordRequired": False,  # TODO: pass
+    }
+    for driver in drivers:
+        d["list"].append(driver.to_dict())
+    # prepare data package
+    response_kwargs['text'] = json.dumps(d)
+    # TODO update device timestamp
+    session.close()
+    return web.Response(**response_kwargs)
