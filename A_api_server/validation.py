ea546817fd17f87a23cf014c355f4acfba444f0a
cosiek <fitek2@o2.pl>
2019-07-06 14:58:09 +0200

    🖧 Use validate as a decorator

--- A_api_server/validation.py
+++ A_api_server/validation.py
@@ -67,6 +67,26 @@ async def validate(request, session, required_params=None):
                 device, params)
 
 
+def view_validation(required_params=None):
+    def outer_wrapper(f):
+        async def inner_wrapper(request):
+            # run validation
+            is_valid, response_kwargs, device, params = await validate(
+                request, request['db_session'], required_params)
+            # update timestamp if valid
+            if is_valid:
+                device.timestamp = params["timestamp"]
+            # pass everything to request
+            request['is_valid'] = is_valid
+            request['response_kwargs'] = response_kwargs
+            request['device'] = device
+            request['params'] = params
+            # run view
+            return await f(request)
+        return inner_wrapper
+    return outer_wrapper
+
+
 def obscure(password):
     m = hmac.new(OBSCURATION_KEY, password.encode('utf-8'), hashlib.md5)
     return m.hexdigest()
