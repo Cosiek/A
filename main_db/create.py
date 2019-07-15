1f8839d6a4c2253aa0a6ef9170345a72c082f950
cosiek <fitek2@o2.pl>
2019-07-16 00:13:31 +0200

    🗄 Add create db script

---
+++ main_db/create.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from db import engine, metadata
+
+# importing table classes
+import calendar
+import infrastructure
+import organization
+
+
+if __name__ == "__main__":
+    print("Create tables")
+    metadata.create_all(engine)
+    print("Populate calendar")
+    calendar.populate_calendar(366 * 4)
